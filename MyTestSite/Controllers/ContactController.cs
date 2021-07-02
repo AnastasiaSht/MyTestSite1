3646e6981cc7ccf574360101d3875fc5a524bb84
shtai <shtai@DESKTOP-41V2N04>
2021-07-02 09:50:19 +0300

    Add project files.

---
+++ MyTestSite/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using MyTestSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace MyTestSite.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/";
+        public ActionResult RenderForm()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "Contact.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(model);
+                TempData["ContactSuccess"] = true;
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            MailMessage message = new MailMessage(model.EmailAddress, "website@installumbraco.web.local");
+            message.Subject = string.Format("Enquiry from {0} {1} - {2}", model.FirstName, model.LastName, model.EmailAddress);
+            message.Body = model.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}
